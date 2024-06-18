9103c27701db8e75b2170a04b51e9e1f34297f99
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-19 00:56:04 +0500

    added ts and js file with compile

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log("hello world!");
+const getUsername = document.querySelector("#user");
+const formSubmit = document.querySelector("#form");
+const main_container = document.querySelector(".main-container");
+// resuable func:
+async function myCustomFetcher(url, options) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(`Network response was not ok - status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("data->", data);
+    return data;
+}
+// define func:
+// let display the card UI:
+const showResultUI = (singleUser) => {
+    const { avatar_url, login, url, location } = singleUser;
+    main_container.insertAdjacentHTML("beforeend", `
+    <div class="card">
+        <img src="${avatar_url}" alt="${login}" />
+        <hr />
+        <div class="card-footer">
+            <img src="${avatar_url}" alt="${login}" />
+            <a href="${url}"> GitHub </a>
+        </div>
+    </div>
+    `);
+};
+const fetchUserData = (url) => {
+    myCustomFetcher(url, {}).then((userInfo) => {
+        for (const singleUser of userInfo) {
+            showResultUI(singleUser);
+        }
+    });
+};
+// default func call:
+fetchUserData("https://api.github.com/users");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,62 @@
-console.log("hello world!");
+const getUsername = document.querySelector("#user") as HTMLInputElement;
+const formSubmit = document.querySelector("#form") as HTMLFormElement;
+const main_container = document.querySelector(".main-container") as HTMLElement;
+
+// so lets define the contract of an object:
+
+interface UserData {
+  id: number;
+  login: string;
+  avatar_url: string;
+  location: string;
+  url: string;
+}
+
+// resuable func:
+
+async function myCustomFetcher<T>(
+  url: string,
+  options: RequestInit
+): Promise<T> {
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok - status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  console.log("data->", data);
+
+  return data;
+}
+
+// define func:
+// let display the card UI:
+
+const showResultUI = (singleUser: UserData) => {
+  const { avatar_url, login, url, location } = singleUser;
+  main_container.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class="card">
+        <img src="${avatar_url}" alt="${login}" />
+        <hr />
+        <div class="card-footer">
+            <img src="${avatar_url}" alt="${login}" />
+            <a href="${url}"> GitHub </a>
+        </div>
+    </div>
+    `
+  );
+};
+
+const fetchUserData = (url: string) => {
+  myCustomFetcher<UserData[]>(url, {}).then((userInfo) => {
+    for (const singleUser of userInfo) {
+      showResultUI(singleUser);
+    }
+  });
+};
+
+// default func call:
+fetchUserData("https://api.github.com/users");
